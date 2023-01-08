ff755b5451a5b33d81e3ea6187a42f726283e00e
Wael <wael_ben_ayed@yahoo.com>
2023-01-08 23:42:58 +0100

    1st commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  const [formData, setFormData] = React.useState(
+    {firstName: "", lastName: ""}
+)
+
+console.log(formData)
+
+function handleChange(event) {
+    setFormData(prevFormData => {
+        return {
+            ...prevFormData,
+            [event.target.name]: event.target.value
+        }
+    })
+}
+
+return (
+    <form>
+        <input
+            type="text"
+            placeholder="First Name"
+            onChange={handleChange}
+            name="firstName"
+        />
+        <input
+            type="text"
+            placeholder="Last Name"
+            onChange={handleChange}
+            name="lastName"
+        />
+    </form>
+)
+}
+
+export default App;
